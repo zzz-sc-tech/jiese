e089f2a84a7e000022399b336558e3cc35b3c442
zzz-sc-tech <18552235272@163.com>
2026-05-30 12:17:19 +0800

    自动保存: 2026-05-30 12:17:19

--- miniprogram/pages/profile/index.js
+++ miniprogram/pages/profile/index.js
@@ -37,7 +37,11 @@ Page({
     remindEnabled: false,
     remindTime: '08:00',
     themeClass: '',
-    themeName: '清新绿'
+    themeName: '清新绿',
+    vibrateIntensity: 'medium',
+    vibrateIntensityName: '中等',
+    vibrateMode: 'auto',
+    vibrateModeName: '自动停止'
   },
 
   onLoad() {
