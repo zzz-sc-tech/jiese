9f0439d3078fe847df42e0336a19526a5da75038
zzz-sc-tech <18552235272@163.com>
2026-06-16 16:48:03 +0800

    自动保存: 2026-06-16 16:48:03

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,6 +1,7 @@
 const api = require('../../utils/api');
 const storage = require('../../utils/storage');
 const dateUtil = require('../../utils/date');
+const ui = require('../../utils/ui');
 
 const app = getApp();
 
