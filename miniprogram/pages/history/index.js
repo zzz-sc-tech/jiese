46673e4c9095ae12366529c97f89eb811756b4da
zzz-sc-tech <18552235272@163.com>
2026-06-17 13:11:44 +0800

    自动保存: 2026-06-17 13:11:44

--- miniprogram/pages/history/index.js
+++ miniprogram/pages/history/index.js
@@ -522,8 +522,8 @@ Page({
     if (data && data.dailyTotals) {
       this.initBarCanvas(data.dailyTotals);
     }
-    // 当有多个时长目标时绘制扇形图
-    if (data && data.goalDistribution && data.goalDistribution.length > 1) {
+    // 当有时长目标时绘制扇形图
+    if (data && data.goalDistribution && data.goalDistribution.length > 0) {
       this.initPieCanvas(data.goalDistribution);
     }
 
